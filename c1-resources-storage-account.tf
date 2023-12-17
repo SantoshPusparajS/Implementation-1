99a658f03dc25fa4883c89371e3c16c317958a53
Santosh Pusparaj S <crowinlearnings@gmail.com>
2023-12-18 00:54:01 +0530

    Final changes done

--- c1-resources-storage-account.tf
+++ c1-resources-storage-account.tf
@@ -1,4 +1,5 @@
 resource "azurerm_storage_account" "vm_storage_account" {
+  depends_on               = [azurerm_resource_group.ansible_rg]
   name                     = var.storage_account_name
   location                 = var.resource_group_location
   resource_group_name      = var.resource_group_name
